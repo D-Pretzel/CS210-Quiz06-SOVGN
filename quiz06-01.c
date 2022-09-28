bb3370a3c2c79eb35e75f8d7bbcadb96f4f0ca2d
David <petzoldavid02@gmail.com>
2022-09-28 14:05:27 -0600

    pointers-r-cool!

--- quiz06-01.c
+++ quiz06-01.c
@@ -1,16 +1,25 @@
 /** quiz06-01.c
 * ===========================================================
-*    Name: 
-* Section:
+*    Name: David Petazold
+* Section: T 6/7
 * Purpose: Quiz 6
 * ===========================================================
 */
 
 #include <stdio.h>
 
+void hoursMinutes(int minutes, int *hours, int *minutesRemainder){
+    *hours = minutes / 60;
+    *minutesRemainder = minutes % 60;
+}
 
 int main(void){
-    
+    int time, hours, minutes;
+    int* hoursptr = &hours;
+    int* minutesptr = &minutes;
+    printf("enter a number of minutes to be converted: ");
+    scanf("%d", &time);
+    hoursMinutes(time, hoursptr, minutesptr);
+    printf("%d minutes is equivalent to %d hours, and %d minutes", time, hours, minutes);
     return 0;
-    
 }
--- quiz06-02.c
+++ quiz06-02.c
@@ -1,18 +1,25 @@
 /** quiz06-02.c
 * ===========================================================
-*    Name: 
-* Section:
+*    Name: David Petzold
+* Section: T6/7
 * Purpose: Quiz 6
 * ===========================================================
 */
- 
+
 #include <stdio.h>
- 
- 
+
+double avgOfDoubles(double array[], int size){
+    double sum = 0;
+    for (int i = 0; i < size; i++){
+        sum += array[i];
+    }
+    return sum / size;
+}
+
 int main(void){
-        
+
     double dailyHighTemp[] = { 63.0, 64.5, 78.1, 80.9, 76.2, 79.0, 82.1 };
-    
+    printf("the average daily high for this week is %f", avgOfDoubles(dailyHighTemp, 7));
     return 0;
 
 }
